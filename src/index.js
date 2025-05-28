17d66910078e49c71ff4ddbdf105f75c6f1b7dbb
alikmpt8529 <m80693827@gmail.com>
2025-05-28 21:46:43 +0900

    test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.jsx'; // 拡張子を明示
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
